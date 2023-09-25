2d2be8e16dd060fbeff8ed5ea3c55cfda743d0be
tttuuu1234 <yidongyi8@gmail.com>
2023-09-25 12:38:57 +0900

    geolocatorで現在地を表示できるようにしてみた

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,73 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Google Maps Demo',
+      home: MapSample(),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-  late GoogleMapController mapController;
+class MapSample extends StatefulWidget {
+  const MapSample({Key? key}) : super(key: key);
 
-  final LatLng _center = const LatLng(-33.86, 151.20);
+  @override
+  State<MapSample> createState() => MapSampleState();
+}
 
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
+class MapSampleState extends State<MapSample> {
+  Position? currentPosition;
+  late GoogleMapController _controller;
+  late StreamSubscription<Position> positionStream;
+  //初期位置
+  final _kGooglePlex = const CameraPosition(
+    target: LatLng(35.604560, 140.123154),
+    zoom: 14,
+  );
+  final locationSettings = const LocationSettings(
+    accuracy: LocationAccuracy.high,
+    distanceFilter: 100,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    Future(() async {
+      final permission = await Geolocator.checkPermission();
+      if (permission == LocationPermission.denied) {
+        await Geolocator.requestPermission();
+      }
+
+      positionStream = Geolocator.getPositionStream(
+        locationSettings: locationSettings,
+      ).listen(
+        (position) {
+          currentPosition = position;
+          print('${position.latitude.toString()}, ${position.longitude.toString()}');
+        },
+      );
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-        useMaterial3: true,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Sydney'),
-          backgroundColor: Colors.green[700],
-        ),
-        body: SafeArea(
-          child: GoogleMap(
-            onMapCreated: _onMapCreated,
-            initialCameraPosition: CameraPosition(
-              target: _center,
-              zoom: 11.0,
-            ),
-            markers: {
-              const Marker(
-                markerId: MarkerId('Sydney'),
-                position: LatLng(-33.86, 151.20),
-                infoWindow: InfoWindow(
-                  title: "Sydney",
-                  snippet: "Capital of New South Wales",
-                ),
-              )
-            },
-          ),
-        ),
-      ),
+    return GoogleMap(
+      mapType: MapType.normal,
+      initialCameraPosition: _kGooglePlex,
+      myLocationEnabled: true,
+      onMapCreated: (GoogleMapController controller) {
+        _controller = controller;
+      },
     );
   }
 }
