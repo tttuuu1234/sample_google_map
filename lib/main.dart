7ead53dea3437edfd0e2a5e2768841c643f6c55d
tttuuu1234 <yidongyi8@gmail.com>
2023-09-25 11:06:11 +0900

    Android_geolocator導入時の設定追加

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,10 @@ class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  State createState() => _MyAppState();
+  State<MyApp> createState() => _MyAppState();
 }
 
-class _MyAppState extends State {
+class _MyAppState extends State<MyApp> {
   late GoogleMapController mapController;
 
   final LatLng _center = const LatLng(-33.86, 151.20);
@@ -22,9 +22,14 @@ class _MyAppState extends State {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        useMaterial3: true,
+      ),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Maps Sample App'),
+          title: const Text('Sydney'),
           backgroundColor: Colors.green[700],
         ),
         body: SafeArea(
@@ -34,6 +39,16 @@ class _MyAppState extends State {
               target: _center,
               zoom: 11.0,
             ),
+            markers: {
+              const Marker(
+                markerId: MarkerId('Sydney'),
+                position: LatLng(-33.86, 151.20),
+                infoWindow: InfoWindow(
+                  title: "Sydney",
+                  snippet: "Capital of New South Wales",
+                ),
+              )
+            },
           ),
         ),
       ),
